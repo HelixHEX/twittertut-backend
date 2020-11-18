2ed49f7e1d65c05e244d415e9568d9c4563eef28
HelixHEX <e.wambugu192@gmail.com>
2020-11-17 23:01:36 -0500

    Removed typeorm logging

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const main = async () => {
   await createConnection({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    logging: true,
+    logging: false,
     synchronize: true,
     migrations: [path.join(__dirname, "./migrations/*")],
     entities: [Tweet, User],
