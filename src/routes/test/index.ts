e785447a34fe958436127e38cf286034a757d992
HelixHEX <heyy.im.eli@gmail.com>
2020-12-03 12:08:10 -0500

    Added test route folder

---
+++ src/routes/test/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const router = express.Router();
+
+router.post('/', (req: express.Request, res: express.Response) => {
+  console.log(req.body);
+  res.json({success: true}).status(200);
+})
