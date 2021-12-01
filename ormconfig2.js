346dcdc674defcd6dea1006399cc6ee40a8c8f84
Elias <e.wambugu192@gmail.com>
2021-11-30 22:42:08 -0800

    test

--- ormconfig2.js
+++ ormconfig2.js
@@ -1,13 +1,13 @@
-module.exports = {
-    "type": "postgres",
-    "port": "5432",
-    "host": "postgres://mrlomrkobbebwl:1821600d828b9ce2922c13be3a85d43dd52087d8fd65ff40746a1d9382fa8c15@ec2-54-147-126-202.compute-1.amazonaws.com",
-    "database": "d21a9a93oshgro",
-    "synchronize": true,
-    "logging": true,
-    "entities": ["dist/entities/*.js"],
-    "migrations": ["dist/migrations/**/*.js"],
-    "cli": {
-      "migrationsDir": "src/migrations"
-    }
-  }
+// module.exports = {
+//     "type": "postgres",
+//     "port": "5432",
+//     "host": "postgres://mrlomrkobbebwl:1821600d828b9ce2922c13be3a85d43dd52087d8fd65ff40746a1d9382fa8c15@ec2-54-147-126-202.compute-1.amazonaws.com",
+//     "database": "d21a9a93oshgro",
+//     "synchronize": true,
+//     "logging": true,
+//     "entities": ["dist/entities/*.js"],
+//     "migrations": ["dist/migrations/**/*.js"],
+//     "cli": {
+//       "migrationsDir": "src/migrations"
+//     }
+//   }
