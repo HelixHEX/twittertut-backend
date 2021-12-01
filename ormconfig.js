850cdd7cd5316643616be6cbec500e2a0630d924
Elias <e.wambugu192@gmail.com>
2021-11-30 22:32:11 -0800

    removed database option

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,6 @@ module.exports = {
     "type": "postgres",
     "port": "5432",
     "host": "postgres://mrlomrkobbebwl:1821600d828b9ce2922c13be3a85d43dd52087d8fd65ff40746a1d9382fa8c15@ec2-54-147-126-202.compute-1.amazonaws.com:5432/d21a9a93oshgro",
-    "database": "twittertut",
     "synchronize": true,
     "logging": true,
     "entities": ["dist/entities/*.js"],
